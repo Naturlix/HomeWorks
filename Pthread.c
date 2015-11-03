2bcbbb9f821b82c2e0e3325e10f303dae3e8b86e
Naturlix <INaturlix@gmail.com>
2015-11-03 10:32:16 +0300

    Create Pthread.c

---
+++ Pthread.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+int X=0;
+int N=100;
+
+void* increm(){
+	int i;
+	for(i=0;i<N;i++) X+=1;	
+}
+
+
+int main(){
+	int i;
+	void* (*x)()=increm;
+	pthread_t thrd[100];
+	
+	for(i=0;i<N;i++){
+	pthread_create(&thrd[i], NULL, x, NULL);	
+	}
+	
+	printf("%d",X);	
+return 0;
+}
